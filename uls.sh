8add603539f8bbd185a3e5b5173ea08a2dff951c
josephworks <smartech56@gmail.com>
2018-05-27 16:22:10 -0700

    added dependencies

--- uls.sh
+++ uls.sh
@@ -1,1 +1,14 @@
 #!/usr/bin/env bash
+
+echo "Starting"
+
+# depends -----------------------------------
+echo "Installing depends..."
+apt install toilet figlet -y
+read -p "Would you like to install additional dependencies? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    sudo apt install #additional depends here
+fi
+# -------------------------------------------
