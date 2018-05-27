0a34b9ab9f3c873caa714811b99ea9ea06e271a6
josephworks <smartech56@gmail.com>
2018-05-27 16:49:54 -0700

    removed useless echo

--- uls.sh
+++ uls.sh
@@ -6,7 +6,6 @@ echo "Starting"
 echo "Installing depends..."
 apt install toilet figlet -y
 read -p "Would you like to install additional dependencies? " -n 1 -r
-echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     sudo apt install #additional depends here
