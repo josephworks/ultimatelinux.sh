e9410f0c0828016d0e937f1713dfd33b68368256
josephworks <smartech56@gmail.com>
2018-05-27 21:27:41 -0700

    added repeat script

---
+++ ulsrepeat.sh
@@ -0,0 +1,57 @@
+read -p "ULS> "
+
+if [[ $REPLY = help ]]
+then
+    echo "Commands:"
+    echo "help - lists commands"
+    echo "update - updates the running linux distro"
+    echo "listpackages - lists all installed packages"
+    echo "install list - used to install applications (apt for beginners)"
+fi
+
+if [[ $REPLY = update ]]
+then
+    sudo aptitude update && sudo aptitude upgrade
+fi
+
+if [[ $REPLY = listpackages ]]
+then
+    dpkg --get-selections
+fi
+
+if [[ $REPLY = install ]]
+then
+    echo "Invalid Usage: install [App]"
+    echo "To see a list of apps you wan install, run "install list""
+fi
+
+if [[ $REPLY = "install list" ]]
+then
+    echo "Installable Apps:"
+    echo "java"
+    echo "maven"
+    echo "npm"
+    echo "nodejs"
+fi
+
+if [[ $REPLY = "install java" ]]
+then
+    sudo apt install default-jre default-jdk -y
+fi
+
+if [[ $REPLY = "install maven" ]]
+then
+    apt install maven -y
+fi
+
+if [[ $REPLY = "install npm" ]]
+then
+    sudo apt install npm -y
+fi
+
+if [[ $REPLY = "install nodejs" ]]
+then
+    sudo apt install npm -y
+fi
+
+sudo bash ulsrepeat.sh
