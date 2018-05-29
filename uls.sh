85ce4113b24e783b1bf00addf3dcaaadc8de6fcc
josephworks <smartech56@gmail.com>
2018-05-29 08:47:35 -0700

    fixed dpkg filename

--- uls.sh
+++ uls.sh
@@ -157,7 +157,7 @@ fi
 if [[ $REPLY = "install webmin" ]]
 then
     wget https://prdownloads.sourceforge.net/webadmin/webmin_1.881_all.deb
-    dpkg -i webmin-current.deb
+    dpkg -i webmin_1.881_all.deb
 fi
 
 sudo bash ulsrepeat.sh
